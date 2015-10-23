c86c0d7cbb719d2e813bfefe33d2009d1370fb98
Zozingzeus <Zozingzeus@gmail.com>
2015-10-24 00:19:08 +0200

    Changed Enemy script back to original due to poor A*, Added Spawner system(needs tweaking or rewrite)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,24 +14,26 @@ public class Enemy : MonoBehaviour {
 
     public Transform target;
     float speed;
-    Vector2[] path;
-    int targetIndex;
 
     public int hp;
     public GameObject projectile;
     public LayerMask whatToHit;
+
+	// Used by Spawner.cs
+	private GameObject objSpawn;
+	private int SpawnerID;
     
 
     // Use this for initialization
     void Start () {
-        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
 
         hp = 30;
         target = GameObject.Find("Player").transform;
         shootRadius = 3;
         walkRadius = 10;
         shootCd = 1f;
-        speed = 1f; 
+        speed = 3f;
+
         // Set arm and gun gameObjects
         foreach (Transform child in transform) {
             if (child.gameObject.name == "Arm") {
@@ -43,22 +45,29 @@ public class Enemy : MonoBehaviour {
                 gun = child.gameObject;
             }
         }
+
+		// Used by Spawner.cs
+		objSpawn = (GameObject) GameObject.FindWithTag ("Spawner");
     }
 	
 	// Update is called once per frame
 	void Update () {
         // Check if dead
-        if (hp <= 0)
-            Destroy(gameObject);
-
+        if (hp <= 0) {
+			// Used by Spawner.cs
+			objSpawn.BroadcastMessage("killEnemy", SpawnerID);
+			Destroy (gameObject);
+		}
 	}
 
     void FixedUpdate() {
 
         // Movement(with animations and arm rotation)
         Vector2 move = target.position - transform.position;
-        /*float rotZ = Mathf.Atan2(move.y, move.x) * Mathf.Rad2Deg;
+        float rotZ = Mathf.Atan2(move.y, move.x) * Mathf.Rad2Deg;
         if (move.magnitude >= shootRadius && move.magnitude <= walkRadius) {
+			move.Normalize();
+			transform.Translate(move * speed * Time.deltaTime);
             GetComponent<Animator>().SetBool("walking", true);
         } else if (move.magnitude <= shootRadius && inLineOfSight) {
             Shoot();
@@ -76,7 +85,6 @@ public class Enemy : MonoBehaviour {
         else
             armScale.x = -1;
         arm.transform.localScale = armScale;
-        */
 
         // Line of sight check
         RaycastHit2D hit = Physics2D.Raycast(transform.position, move, walkRadius, whatToHit);
@@ -98,30 +106,6 @@ public class Enemy : MonoBehaviour {
         }
     }
 
-    public void OnPathFound(Vector2[] newPath, bool pathSuccessful) {
-        if (pathSuccessful) {
-            path = newPath;
-            StopCoroutine("FollowPath");
-            StartCoroutine("FollowPath");
-        }
-    }
-
-    IEnumerator FollowPath() {
-        Vector2 currentWaypoint = path[0];
-
-        while (true) {
-            if ((Vector2)transform.position == currentWaypoint) {
-                targetIndex++;
-                if (targetIndex >= path.Length) {
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
-
-            transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-        }
-    }
-
     public void TakeDamage(int damage) {
         hp -= damage;
     }
@@ -134,4 +118,9 @@ public class Enemy : MonoBehaviour {
         if(inLineOfSight) Debug.DrawLine(transform.position, target.position, Color.green);
         else Debug.DrawLine(transform.position, target.position, Color.red);
     }
+
+	// Used by Spawner.cs
+	void setName(int sName) {
+		SpawnerID = sName;
+	}
 }
